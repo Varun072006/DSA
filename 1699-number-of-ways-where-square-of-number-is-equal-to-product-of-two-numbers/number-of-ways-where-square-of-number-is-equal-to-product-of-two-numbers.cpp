120816cfe5b2646d843922590a50dc4ffe45a92e
Varun S <varunshanmugam643@gmail.com>
2026-09-12 21:34:05 +0530

    Time: 7 ms (83.20%) | Memory: 15.1 MB (63.11%) - LeetSync

--- 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
+++ 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
-    int numTriplets(vector<int>& n1, vector<int>& n2) {
-    return accumulate(begin(n1), end(n1), 0, [&](int s, long n) { return s + twoProduct(n * n, n2); }) 
-        + accumulate(begin(n2), end(n2), 0, [&](int s, long n) { return s +  twoProduct(n * n, n1); });
-}
-int twoProduct(long i, vector<int> &n) {
-    unordered_map<int, int> m;
-    int cnt = 0;
-    for (auto v : n) {
-        if (i % v == 0)
-            cnt += m[i / v];
-        ++m[v];
+    int numTriplets(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<long long, long long> freq1, freq2;
+        
+        for (int x : nums1) ++freq1[x];
+        for (int x : nums2) ++freq2[x];
+        
+        auto countWays = [](const vector<int>& source,
+                            const unordered_map<long long, long long>& targetFreq) {
+            long long result = 0;
+            
+            for (long long x : source) {
+                long long square = x * x;
+                
+                for (const auto& [value, count] : targetFreq) {
+                    if (square % value != 0) continue;
+                    
+                    long long other = square / value;
+                    auto it = targetFreq.find(other);
+                    if (it == targetFreq.end()) continue;
+                    
+                    if (value < other) {
+                        result += count * it->second;
+                    } else if (value == other) {
+                        result += count * (count - 1) / 2;
+                    }
+                }
+            }
+            
+            return result;
+        };
+        
+        long long answer = countWays(nums1, freq2) + countWays(nums2, freq1);
+        return static_cast<int>(answer);
     }
-    return cnt;
-} 
 };
