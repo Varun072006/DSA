0bdab568b2e08c334d29894a70a1c0dc8845454b
Varun S <varunshanmugam643@gmail.com>
2026-09-12 21:32:36 +0530

    Time: 10 ms (99.35%) | Memory: 45.5 MB (60.00%) - LeetSync

--- 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
+++ 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -1,20 +1,58 @@
 class Solution {
-    public int numTriplets(int[] n1, int[] n2) {
-    long res = 0;
-    for (long n : n1)
-        res += twoProduct(n * n, n2);
-    for (long n : n2)
-        res += twoProduct(n * n, n1);
-    return (int)res;
-}
-long twoProduct(long i, int[] n) {
-    Map<Long, Long> m = new HashMap<>();
-    long cnt = 0;
-    for (long v : n) {
-        if (i % v == 0)
-            cnt += m.getOrDefault(i / v, 0l);
-        m.put(v, m.getOrDefault(v, 0l) + 1);
+    public int numTriplets(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int count = 0;
+        count += triplets(nums1, nums2);
+        count += triplets(nums2, nums1);
+        
+        return count;        
     }
-    return cnt;
-}  
+    
+    public int triplets(int[] nums1, int[] nums2) {
+        
+		Map<Long, Integer> seen = new HashMap<>();
+        int count = 0;
+        
+        for(int i=0; i<nums1.length; i++) {
+            long sqr = (long) nums1[i] * nums1[i]; 
+            if(seen.containsKey(sqr)) {  
+                count += seen.get(sqr);
+                continue;
+            }
+            
+            int localCount = 0; 
+            int left = 0, right = nums2.length-1; 
+			
+            while(left < right) {
+                if(nums2[left] > sqr)
+                    break;
+                long multiple = (long) nums2[left] * nums2[right];
+                
+                if(sqr == multiple) {
+                    if(nums2[left] == nums2[right]) { 
+						int n = right - left+1;
+                        localCount += n * (n-1)/2;
+                        break;
+                    }
+                    localCount++;
+                    int newL = left+1;
+                    for(newL = left+1; newL < right && nums2[newL] == nums2[left]; newL++) {
+                            localCount++;                        
+                    }
+                    right--;
+                    
+                } else if (multiple < sqr)
+                    left++;
+                else
+                    right--;
+            }
+			
+			count += localCount;
+            seen.put(sqr, localCount);
+			
+        }
+        return count;
+    }
 }
