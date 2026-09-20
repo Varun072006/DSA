cb1903c716d7a924aa076617f98bd594981cd8ec
Varun S <varunshanmugam643@gmail.com>
2026-09-20 22:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
-    int reverseDegree(string& s) {
-        int sum=0, n=s.size();
-        for(int i=0; i<n; i++) sum+=(i+1)*('z'-s[i]+1);
+    int reverseDegree(string s) {
+        int sum = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            sum += (i+1)*(26-(s[i]-'a'));
+        }
         return sum;
     }
 };
