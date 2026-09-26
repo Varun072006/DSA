5ad79e84a939d8120c864007e3a9840c9c19fba7
Varun S <varunshanmugam643@gmail.com>
2026-09-26 21:24:57 +0530

    Time: 36 ms (100.00%) | Memory: 302 MB (6.23%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,21 +1,69 @@
+struct TrieNode {
+    TrieNode* next[26];
+    int idx=-1;
+};
+static constexpr int N=1e6+1;  
+static TrieNode pool[N];
+static int ptr=0;
+struct Trie {
+    TrieNode* newNode() {
+        TrieNode* node=&pool[ptr++];
+        memset(node->next, 0, sizeof(node->next));
+        node->idx=-1;
+        return node;
+    }
+    TrieNode* root;
+    Trie(){
+        ptr=0;
+        root=newNode();
+    }
+    void insert(string_view word, int id) {
+        TrieNode* Node=root;
+        for(char c: word){
+            int i=c-'a';
+            if(Node->next[i]==NULL)
+                Node->next[i]=newNode();
+            Node=Node->next[i];
+        }
+        Node->idx=id;
+    }
+    
+    int search(string_view s) {
+        TrieNode* Node=root;
+        for(char c : s){
+            int i=c-'a';
+            if(Node->next[i]==NULL) return -1;
+            Node=Node->next[i];
+        }
+        return Node->idx;
+    }
+};
 class Solution {
 public:
-    string evaluate(string s, auto& K) {
-        unordered_map<string, string> d;
-        for (auto& k : K)
-            d[k[0]] = k[1];
+    string evaluate(string& s, vector<vector<string>>& knowledge) {
+        const int n=s.size(), m=knowledge.size();
+        Trie trie;
+        for(int i=0; i<m; i++)
+            trie.insert(knowledge[i][0], i);
 
-        string res;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') {
-                int j = s.find(")", i + 1);
-                auto t = s.substr(i + 1, j - i - 1);
-                res += d.count(t) ? d[t] : "?";
-                i = j;
-            } else
-                res += s[i];
+        string t;
+        t.reserve(n);
+        for(int i=0; i<n; i++){
+            while(i<n && s[i]>='a')
+                t+=s[i++];
+            if (i==n) break;
+
+            if (s[i]=='('){
+                int l=i++, r;
+                while(i<n && s[i]>='a') i++;
+                if (s[i]==')') r=i;
+                string key=s.substr(l+1, r-l-1);
+                int id=trie.search(key);
+                if (id!=-1)
+                    t+=knowledge[id][1];
+                else t+='?';
+            }
         }
-
-        return res;
+        return t;
     }
 };
